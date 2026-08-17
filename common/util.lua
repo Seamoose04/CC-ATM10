cfa1265d356eab94f02edea405da92311417ce19
seamoose04 <seamusjohnson04@gmail.com>
2026-08-17 15:36:54 -0400

    Added string utilities and common monitor functions

--- common/util.lua
+++ common/util.lua
@@ -3,6 +3,59 @@
 ---@class Util
 local util = {}
 
+---@param n number
+---@return number rounded
+function util.round(n)
+	return math.floor(n + 0.5)
+end
+
+---@param n number Must be positive
+---@param decimals integer
+---@return string abbreviated
+function util.abbreviateNumber(n, decimals)
+	local shorthands = { "k", "m", "b", "t" }
+	local shortened = n
+	local degree = 0
+	local precision = 10 ^ decimals
+
+	while shortened / 1000 >= 1 do
+		shortened = shortened / 1000
+		degree = degree + 1
+	end
+
+	if degree == 0 then
+		return tostring(util.round(shortened * precision) / precision)
+	end
+
+	if degree > #shorthands then
+		print("Warning: number too large to abbreviate.")
+		return tostring(util.round(shortened * precision) / precision) .. "?"
+	end
+	return tostring(util.round(shortened * precision) / precision) .. shorthands[degree]
+end
+
+---@param str string
+---@param width integer
+---@return string padded
+function util.padStart(str, width)
+	local padding = width - #str
+	if padding <= 0 then
+		return str
+	end
+	return string.rep(" ", padding) .. str
+end
+
+---@param str string
+---@param width integer
+---@return string padded
+function util.padEnd(str, width)
+	local padding = width - #str
+	if padding <= 0 then
+		return str
+	end
+	return str .. string.rep(" ", padding)
+end
+
 ---@param t table
 ---@return number
 function util.tableCount(t)
